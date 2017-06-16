fabfd229d1bd16e9e10d827f787bec62ec4f1fd4
Patricia <patsmrkt@gmail.com>
2017-06-16 15:56:10 -0400

    Display and calculate user stats
    Popup box appears with users wpm and time taken

--- typeracer.js
+++ typeracer.js
@@ -1,3 +1,23 @@
+function timer(){
+  var id = setInterval(frame, 500);
+  var userInput = document.getElementById("user-input");
+  var str = "The quick brown fox jumps over the lazy dog!"
+  var time = 0;
+
+  function frame() {
+    if (str === userInput.value){
+      displayStats(time);
+      clearInterval(id);
+    }
+    else
+      time += 0.5;
+  }
+}
+
+function enable() {
+  document.getElementById("user-input").disabled = false;
+}
+
 function onKeyPress() {
     // Create array containing passage
     var str = "The quick brown fox jumps over the lazy dog!";
@@ -16,6 +36,10 @@ function onKeyPress() {
       else correctInput(userInput);
     }
 
+    if (str === userInput.value){
+      userInput.disabled = true;
+    }
+
 }
 
 function incorrectInput(userInput) {
@@ -26,4 +50,18 @@ function incorrectInput(userInput) {
 function correctInput(userInput) {
   userInput.style.color = "green";
   userInput.style.backgroundColor = "white";
+}
+
+function displayStats(time){
+  var popup = document.getElementById("stats");
+  var wpmStat = document.getElementById("wpm");
+  var timeStat = document.getElementById("time");
+  var title = document.getElementById("done");
+  var wpm = 8 / time * 60;
+
+  title.innerHTML = "good job!";
+  wpmStat.innerHTML = "WPM: " + wpm;
+  timeStat.innerHTML = "Time: " + time + "s";
+
+  popup.classList.toggle("show");
 }
